01478f51da79e04720f89cb0162b21a30739cff0
Iznore <iznore147@gmail.com>
2017-07-11 14:20:24 +0900

    update 20170711 ina226.py

--- Python_sourcefiles/ina226_python.py
+++ Python_sourcefiles/ina226_python.py
@@ -1,35 +1,49 @@
 import RPi.GPIO as GPIO
 import time
 import commands
+import datetime
+
+vltg = []
+crnt = []
+calctime = []
 
 def GetV():
-check = commands.getoutput("i2cget -y 1 0x4f 0x02 w")
-return (int(check[4:6],16)*256+int(check[2:4],16))*1.25/1000
+    check = commands.getoutput("i2cget -y 1 0x4f 0x02 w")
+    return (int(check[4:6],16)*256+int(check[2:4],16))*1.25/1000
 
-def GetA():
-check = commands.getoutput("i2cget -y 1 0x4f 0x04 w")
-if int(check[4:6],16)<128:
-return (int(check[4:6],16)*256+int(check[2:4],16))*0.1
-else:
-return (int(check[4:6],16)*256+int(check[2:4],16)-256*256)*0.1
+    def GetA():
+        check = commands.getoutput("i2cget -y 1 0x4f 0x01 w")
+        if int(check[4:6],16)<128:
+            return (int(check[4:6],16)*256+int(check[2:4],16))*0.1
+        else:
+            return (int(check[4:6],16)*256+int(check[2:4],16)-256*256)*0.1
 
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(5,GPIO.OUT)
-
-check = commands.getoutput("i2cset -y 1 0x4f 0x05 0x0a 0x00 i")
+check = commands.getoutput("i2cset -y 1 0x4f 0x00 0x40 0x07 i")
 time.sleep(1)
-print str(GetV())+" V, "+str(GetA())+" mA "
 
 GPIO.output(5,GPIO.HIGH)
 print "LED-on"
 time.sleep(1)
-print str(GetV())+" V, "+str(GetA())+" mA "
+
+start = time.time()
+
+while time.time() - start < 1.0 :
+    vltg.append(GetV())
+    crnt.append(GetA())
+    now = datetime.datetime.now()
+    calctime.append(now.strftime("%H:%M:%S.") + "%04d" % (now.microsecond // 1000))
+    sleep(0.1)
+
 
 GPIO.output(5,GPIO.LOW)
 print "LED-off"
 time.sleep(1)
-print str(GetV())+" V, "+str(GetA())+" mA "
+
+for num in range(len(vltg)) :
+    print calctime[num] + str(vltg[num]) + " V, " + str(crnt[num])+" mA "
 
 GPIO.cleanup()
 exit
