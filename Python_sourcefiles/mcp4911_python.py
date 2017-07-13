2a376228b5aca82f32c61708fd080ac175a1292e
Iznore <iznore147@gmail.com>
2017-07-13 15:23:23 +0900

    no message

--- Python_sourcefiles/mcp4911_python.py
+++ Python_sourcefiles/mcp4911_python.py
@@ -1,2 +1,32 @@
 import spidev
 import time
+import RPi.GPIO as GPIO
+
+def SetDAChighbyte(value):
+    return highbyte = value >> 8
+
+def SetDAClowbyte(value):
+    return lowbyte = value & 0xff
+
+# ADCが接続されているチャネル
+mcp_channel = 0
+
+#LED GPIO
+GPIO.setmode(GPIO.BCM)
+led = 5
+GPIO.setup(led, GPIO.OUT)
+
+#SPIバスへのアクセスを開く
+spi = spidev.SpiDev()
+spi.open(0, 0)
+
+value = 500
+
+try:
+    while True:
+        r = spi.xfer2([(0x30 | SetDAChighbyte(value)), SetDAClowbyte(value)])
+        time.sleep(1)
+except KeyboardInterrupt:
+    spi.close()
+
+GPIO.cleanup()
